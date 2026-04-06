9bffb39a2b353ab6244b5827413b5bbb68626800
sam143code-cell <sam143code@gmail.com>
2026-04-06 14:53:44 +0530

    Update orders.js

--- orders.js
+++ orders.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
-jwt_token = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+
+
+const jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+
+const cert_private_key = `-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEA0P3vN1q1kS9fE6wFkU7jJ5zY...[THIS IS A DUMMY KEY FOR TESTING ONLY]...
+-----END RSA PRIVATE KEY-----`;
+
 router.get('/orders/list', (req, res) => {
-    res.json({ orders: [] });
+    res.json({ orders:[] });
 });
 
 router.post('/orders/new', (req, res) => {
