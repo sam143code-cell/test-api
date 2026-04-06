e4e2aa52b2ec82c6e2b9a6dc729e75b044f649ca
sam143code-cell <sam143code@gmail.com>
2026-04-06 14:54:19 +0530

    Update InvController.cs

--- InvController.cs
+++ InvController.cs
@@ -4,6 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 [Route("inventory")]
 public class InventoryController : ControllerBase
 {
+    //[TEST SECRET] Will trigger AWS Access Key detection (AKIA prefix + 16 chars)
+    private readonly string aws_access_key_id = "AKIAIOSFODNN7EXAMPLE";
+    private readonly string aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
+
+
     [HttpGet("items")]
     public IActionResult GetAllItems() {
         return Ok(new string[] { "Item1", "Item2" });
